fbf9912ddd6dafd2d173c964fd54899544d1a124
RenskeSteenbrink <70508688+RenskeSteenbrink@users.noreply.github.com>
2020-10-08 14:34:54 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,5 @@
+// JavaScript Document
+
 var navElement = document.querySelector("header nav");
 var hamburgerMenuButton = document.querySelector("header nav a");
 var burgerMenu = document.querySelector("header nav img");
@@ -7,7 +9,7 @@ function showMenu() {
 	if (navElement.classList.contains("showMenu")) {
 	burgerMenu.src = "images/ICONS/clear-24px.svg";
 	} else {
-	burgermenu.src = "images/ICONS/clear-24px.svg";
+	burgerMenu.src = "images/ICONS/menu-24px.svg";
 	}
 }
 
