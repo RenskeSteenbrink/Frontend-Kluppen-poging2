204e403e442de7a6a18c9b7889474f79693bfc6d
RenskeSteenbrink <70508688+RenskeSteenbrink@users.noreply.github.com>
2020-10-22 14:03:30 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,16 @@
+// JavaScript Document
 
 var navElement = document.querySelector("header nav");
 var hamburgerMenuButton = document.querySelector("header nav a");
 var burgerMenu = document.querySelector("header nav img");
 
-function showMenu(event) {
-	event.preventDefault(); //zorgt dat die link niet actief is maar dat de code daaronder het afhandeld
+function showMenu() {
 	navElement.classList.toggle("showMenu");
-		if (navElement.classList.contains("showMenu")) {
-		burgerMenu.src = "images/ICONS/clear-24px.svg";
-		} else {
-		burgerMenu.src = "images/ICONS/menu-24px.svg";
-		}
+	if (navElement.classList.contains("showMenu")) {
+	burgerMenu.src = "images/ICONS/clear-24px.svg";
+	} else {
+	burgerMenu.src = "images/ICONS/menu-24px.svg";
+	}
 }
 
 hamburgerMenuButton.addEventListener("click", showMenu);
-
